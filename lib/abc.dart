6734a8cb903dc59b43018c359f90ae937ec9aab3
Lai Hoang Viet <laihoangv@gmail.com>
2020-08-27 11:49:44 +0700

    xcczxcxzczczczcczczczczxc

---
+++ lib/abc.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class Viet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(''),
+        ),
+        body: Center(
+          child: Text(''),
+        ),
+      ),
+    );
+  }
+}
