114d50383e49137f958de262a2e93ed4aa85b983
Lai Hoang Viet <laihoangv@gmail.com>
2020-08-27 10:14:03 +0700

    2 commit

--- lib/ListTopic.dart
+++ lib/ListTopic.dart
@@ -5,12 +5,111 @@ import 'package:flutter/widgets.dart';
 class Listopic extends StatelessWidget {
   List<Widget> containers = [
     Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          width: 1,
-        ),
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        children: <Widget>[
+          Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      ClipOval(
+                        child: Image.asset(
+                          'images/anh1.jpg',
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 15),
+                              child: Text(
+                                'Lại Hoàng Việt',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              '100 - ***',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(
+                    bottom: 10,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      RaisedButton(
+                        onPressed: () {},
+                        color: Colors.blue,
+                        child: Text('Test'),
+                        textColor: Colors.white,
+                        highlightElevation: 4.0,
+                      ),
+                      RaisedButton(
+                        onPressed: () {},
+                        color: Colors.blue,
+                        child: Text('Rank'),
+                        textColor: Colors.white,
+                        highlightElevation: 4.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      new Image.asset(
+                        'images/anh1.jpg',
+                        width: 150,
+                        height: 150,
+                      ),
+                      Container(
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              'Lại Hoàng Việt',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '100 - ***',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-
     ),
     Container(
       child: Text('hoang'),
